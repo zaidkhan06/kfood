e12836ce9837b0fe8985191de956d8511738b592
Zaid Khan <zk2508310@email.com>
2025-09-24 18:34:34 +0530

    setup and intigrate tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='h-screen bg-gray-100'>
+      <h1 className='text-6xl font-semibold text-orange-400  flex justify-center '>KFood</h1>
+    </div>
+  )
+}
+
+export default App
