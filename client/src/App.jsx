9832bfb749c0b4183c1fe5417b18f77b28e4a6cb
Khan Construction <naved2508310@gmail.com>
2025-09-26 00:08:15 +0530

    Updated Navbar and Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import { Route, Routes } from "react-router-dom"
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/SignIn"
+import Navbar from "./components/Navbar"
+import Home from "./components/Home";
 
 
 function App() {
   
 
   return (
+    <>
+    <Navbar />
     <Routes>
-       <Route path="/" element={<SignUp />} />
+       <Route path="/" element={<Home/>} />
+       <Route path="/signup" element={<SignUp />} />
        <Route path="/signin" element={<SignIn />} />
     </Routes>
+    </>
   )
 }
 
