c070d3690e7de218635d1be3042c50e5ca7bf1a9
Zaid Khan <zk2508310@email.com>
2025-09-26 21:27:44 +0530

    BACKEND INTIGRATION

--- server/index.js
+++ server/index.js
@@ -10,10 +10,13 @@ import userRouter from "./routes/user.routes.js";
 
 const app = express()
 const port = process.env.PORT || 5000;
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://kfood-alpha.vercel.app"],
+    credentials: true,
+  })
+);
+
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRouter)
