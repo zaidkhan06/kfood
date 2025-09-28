7bfa346de4289f63b4dbd5229f201b24d16b5cce
Zaid Khan <zk2508310@email.com>
2025-09-28 22:16:06 +0530

    bu fix

--- server/index.js
+++ server/index.js
@@ -14,10 +14,11 @@ const app = express()
 const port = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://kfood-alpha.vercel.app"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
+
 
 app.use(express.json())
 app.use(cookieParser())
