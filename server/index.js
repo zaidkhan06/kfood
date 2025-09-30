26accd0093b82e722bc66301f4446566f3d79bc2
Zaid Khan <zk2508310@email.com>
2025-09-30 18:31:28 +0530

    .

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
+    methods:["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "CONNECT", "TRACE"]
   })
 );
 
